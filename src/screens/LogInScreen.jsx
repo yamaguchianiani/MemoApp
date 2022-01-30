a33f739644da6be7c9b9a1e475d13db56fed4825
aniani <84556449+yamaguchianiani@users.noreply.github.com>
2022-01-31 08:54:21 +0900

    Set TextInput options

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import {
     View,Text,TextInput,StyleSheet,TouchableOpacity,
 } from "react-native";
@@ -8,13 +8,29 @@ import Button from "../components/Button";
 
 export default function LogInScreen(props){
     const {navigation}=props;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return(
         <View style={styles.container}>
 
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
-                <TextInput style={styles.input} value="Email Adress" />
-                <TextInput style={styles.input} value="Password" />
+                <TextInput style={styles.input}
+                value={email}
+                onChangeText={(text) => {setEmail(text);}}
+                autoCapitalize='none'
+                keyboardType="email-address"
+                placeholder="Email Address"
+                textContentType='emailAddress'
+                />
+                <TextInput style={styles.input}
+                value={password}
+                onChangeText={(text) => {setPassword(text);}}
+                autoCapitalize="none"
+                placeholder='Password'
+                secureTextEntry
+                textContentType='password'
+                />
                 <Button label="Submit"
                 onPress={()=>{navigation.reset({
                     index:0,
