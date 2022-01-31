59c2842018ad839d2c0d82e2c5bf9dcb303aeb51
aniani <84556449+yamaguchianiani@users.noreply.github.com>
2022-02-01 07:19:12 +0900

    Auto login and logout button

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import {
     View,Text,TextInput,StyleSheet,TouchableOpacity,Alert,
 } from "react-native";
@@ -11,6 +11,22 @@ export default function LogInScreen(props){
     const {navigation}=props;
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    useEffect(()=> {
+        const unsubscribe=firebase.auth().onAuthStateChanged((user)=>{
+            if(user){
+                navigation.reset({
+                    index:0,
+                    routes:[{ name:"MemoList"}],
+                });
+
+            }
+
+
+        });
+        return unsubscribe;
+    },[] );
+
     function handlePress(){
         firebase.auth().signInWithEmailAndPassword(email,password)
         .then((userCredential)=>{
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {View, StyleSheet} from "react-native";
 
 import MemoList from "../components/MemoList";
 import CircleButton from "../components/CircleButton";
+import LogOutButton from "../components/LogOutButton";
+
 
 
 export default function MemoListScreen(props){
     const {navigation}=props;
+    useEffect(()=>{
+        navigation.setOptions({
+            headerRight: ()=> <LogOutButton/>,
+        });
+
+    },[]);
+
     return(
         <View style={styles.container}>
 
